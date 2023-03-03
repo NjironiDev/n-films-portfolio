8171e6eb37d1e6994a32c6d1f3a639c3ee43f9b6
NassimKach <nassimkackac@gmail.com>
2023-03-03 02:10:56 +0100

    add services section 📦

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import DemoComponent from "@/components/DemoComponent";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -13,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <DemoComponent />
+        <Services />
       </main>
     </>
   );
